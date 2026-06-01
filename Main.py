71a69007c27ea82cea9ad320e87d0277549f9b48
brustemzade10-glitch <brustemzade10@gmail.com>
2026-06-01 19:41:16 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,18 @@
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+@app.route('/')
+def home():
+    return "Bot aktivdir!"
+
+def run():
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
+
+Thread(target=run).start()
+# --------------------------------------------------
+
 import telebot
 import requests
 import urllib.parse
